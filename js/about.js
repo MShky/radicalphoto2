a593da3740ed4c410dc0ccc8b0ddd8b8ee242662
Mshky <mariannesharkey@gmail.com>
2015-12-10 09:17:26 -0500

    Please let me know if you got it!

---
+++ js/about.js
@@ -0,0 +1,109 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// load .content_area
+// sheila.html / frank.html / joanne.html
+// Ajax 
+
+
+//$("#sheila").click (function sheilaContent()){
+//  	html.load("sheila.html, #bio");
+//  }
+
+//$(document).ready(function(){
+//	$("#sheila").click(function(e){
+//		e.preventDefault();
+//		var link_url= $(this).attr("href");
+////		alert(link_url);
+//		$("#content_area").load(link_url +"#bio");
+//	});		
+//});
+
+//$(document).ready(function(){
+//  $("button").click(function(){
+//    $.ajax({url:"myPage2.html",success:function(result){
+//      $("#div1").html(result);
+//    }});
+//  });
+//});
+
+
+
+//$(document).ready(function () {
+//    $('#sheila').click(function (e) {
+//		e.preventDefault(e);
+//        var url = $(this).attr('href');
+//        $.ajax({
+//            url: sheila.html
+//        }).done(function (result) {
+//            $('#bio').html(result);
+//        });
+//    });
+
+
+
+
+
+
+//		$(document).ready(function(){
+//			$("#sheila").mouseover(function(e){
+//				e.preventDefault();
+//				var link_url= $(this).attr("href");
+//				$("#content_area").load(link_url +"#bio");
+//			});		
+//		});
+
+
+
+
+
+
+
+
+//Contact 
+//<!--
+//		<?php
+//		// user information
+//			$user_name='';
+//		// check user input
+//		  if (isset($_POST['user_name'])){
+//		   $user_name = $_POST['user_name'];
+//		   $user_name = strip_tags($user_name);
+//		``$user_name = htmlspecialchars($user_name);
+//		   $user_name = strtolower($user_name);
+//		   $user_name = ucwords($user_name);
+//		  	}else{
+//				$user_name='';
+//			}
+//		// user information
+//			$user_email='';			
+//		// check user input
+//			if(isset($_POST['user_email'])){
+//			$user_email = $_POST ['user_email'];
+//			$user_email = strtolower($user_email);
+//			$user_email = strip_tags($user_email);
+//			}else{
+//				$user_email='';
+//			}
+//		//user message
+//			$user_message='';	
+//		// check user input
+//			if (isset($_POST['user_message'])){
+//			$user_message = $_POST ['user_message'];
+//			$user_message = htmlspecialchars($user_message);
+//			}else{
+//				$user_message='';	
+//			}
+//		?php>
+//-->
